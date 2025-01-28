4e1ec7f28307c7fe5fb713526a6dbbd98aad8f0b
sharmaabhijith <sharmaabhijith1@gmail.com>
2025-01-28 13:28:58 +0000

    make_datasetv2

--- Helpers/make_dataset.py
+++ Helpers/make_dataset.py
@@ -1,6 +1,9 @@
 import os
 import sys
 sys.path.append("../")
+import requests
+import tarfile
+
 
 DATASET_NAME = "imagenette"
 ROOT_DIR = "../dataset"
@@ -10,6 +13,24 @@ SPLITS = ["train", "test"]
 
 
 def retrieve_data(DATASET_NAME, ROOT_DIR, url):
+    output_file = os.path.join(ROOT_DIR, f"{DATASET_NAME}2.tgz")
+    RAW_DATA_DIR =  os.path.join(ROOT_DIR, f"{DATASET_NAME}2")
+    if os.path.exists(os.path.join(ROOT_DIR, DATASET_NAME)):
+        print(f"Dataset already present at : {ROOT_DIR}")
+    else:
+        if not os.path.exists(output_file):
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                with open(output_file, "wb") as file:
+                    for chunk in response.iter_content(chunk_size=1024):
+                        file.write(chunk)
+                print(f"Downloaded {output_file}")
+            else:
+                print(f"Failed to download. Status code: {response.status_code}")
+        if not os.path.exists(RAW_DATA_DIR):
+            with tarfile.open(output_file, "r:gz") as tar:
+                tar.extractall(path=ROOT_DIR)
+            print(f"Extracted files to {ROOT_DIR}")
 
 
 if DATASET_NAME=="imagenette":
@@ -17,14 +38,17 @@ if DATASET_NAME=="imagenette":
         "n01440764", "n02102040", "n02979186", "n03000684", "n03028079", 
         "n03394916", "n03417042", "n03425413", "n03445777", "n03888257"
     ]
+    url = "https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz"
 elif DATASET_NAME=="imagewoof":
     raw_labels = [
         "n02086240", "n02087394", "n02088364", "n02089973", "n02093754", 
         "n02096294", "n02099601",  "n02105641", "n02111889", "n02115641"
     ]
+    url = "https://s3.amazonaws.com/fast-ai-imageclas/imagewoof2.tgz"
 
+retrieve_data(DATASET_NAME, ROOT_DIR, url)
 
-train_folders = sorted(glob(os.path.join(RAW_DATA_DIR, "*")))
+train_folders = sorted(glob(os.path.join(RAW_DATA_DIR, "train", "*")))
 
 class_maps = {}
 for i in range(raw_labels):
@@ -41,7 +65,7 @@ for sp in SPLITS:
     os.makedirs(os.path.join(DATA_DIR, sp, cls), exist_ok=True)
 
 for i, cls_index in enumerate(class_indices):
-  image_paths = np.array(glob(f'{train_folders[class_indices [cls_index]]}/*.JPEG'))
+  image_paths = np.array(glob(os.path.join(f"{train_folders[class_names[i]]}","*.JPEG"))
   class_idx = class_indices[i]
   print(f'{class_idx}: {len(image_paths)}')
   np.random.shuffle(image_paths)
